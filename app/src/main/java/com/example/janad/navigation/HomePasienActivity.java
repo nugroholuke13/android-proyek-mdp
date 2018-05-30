17ba8ef474a10b98f278e879a9a3ddc9955cca5a
Luke Nugroho <nugroholuke13@gmail.com>
2018-05-30 14:00:21 +0700

    Commit Pasien

---
+++ app/src/main/java/com/example/janad/navigation/HomePasienActivity.java
@@ -0,0 +1,13 @@
+package com.example.janad.navigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePasienActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_pasien);
+    }
+}
