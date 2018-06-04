38ca13add7b520d22e5334bf7042d89b6b0e5c3b
Luke Nugroho <nugroholuke13@gmail.com>
2018-06-04 22:36:50 +0700

    Tambahan Navigation Drawer
    HomeDokter dan HomePasien

--- app/src/main/java/com/example/janad/navigation/HomeDokterActivity.java
+++ app/src/main/java/com/example/janad/navigation/HomeDokterActivity.java
@@ -2,65 +2,133 @@ package com.example.janad.navigation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeDokterActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
-    Button btnJadwal,btnLibur,btnBatal,btnOffline,btnLive;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_dokter);
-        btnJadwal = findViewById(R.id.button1);
-        btnLibur = findViewById(R.id.button2);
-        btnBatal = findViewById(R.id.button3);
-        btnOffline = findViewById(R.id.button4);
-        btnLive = findViewById(R.id.button5);
-
-
-        btnJadwal.setOnClickListener(new View.OnClickListener() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Menginisiasi  NavigationView
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        //Mengatur Navigasi View Item yang akan dipanggil untuk menangani item klik menu navigasi
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            // This method will trigger on item Click of navigation menu
             @Override
-            public void onClick(View v) {
-                Intent jdwlDokter = new Intent(HomeDokterActivity.this,jadwalDokter.class);
-                startActivity(jdwlDokter);
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                //Memeriksa apakah item tersebut dalam keadaan dicek  atau tidak,
+                if(menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+                //Menutup  drawer item klik
+                drawerLayout.closeDrawers();
+                //Memeriksa untuk melihat item yang akan dilklik dan melalukan aksi
+                switch (menuItem.getItemId()){
+                    // pilihan menu item navigasi akan menampilkan pesan toast klik kalian bisa menggantinya
+                    //dengan intent activity
+                    case R.id.navigation1:
+                        Intent intent1 = new Intent(HomeDokterActivity.this, HomeDokterActivity.class);
+                        startActivity(intent1);
+                        return true;
+                    case R.id.navigation2:
+                        Intent intent2 = new Intent(HomeDokterActivity.this, jadwalDokter.class);
+                        startActivity(intent2);
+                        return true;
+                    case R.id.navigation7:
+                        Intent intent3 = new Intent(HomeDokterActivity.this, jadwalLiburDokter.class);
+                        startActivity(intent3);
+                        return true;
+                    case R.id.navigation9:
+                        Intent intent4 = new Intent(HomeDokterActivity.this, pembatalan.class);
+                        startActivity(intent4);
+                        return true;
+                    case R.id.navigation3:
+                        Intent intent5 = new Intent(HomeDokterActivity.this, jadwalLive.class);
+                        startActivity(intent5);
+                        return true;
+                    case R.id.navigation8:
+                        Intent intent6 = new Intent(HomeDokterActivity.this, jadwalOffline.class);
+                        startActivity(intent6);
+                        return true;
+                    case R.id.navigation4:
+                        Intent intent7 = new Intent(HomeDokterActivity.this, HomeDokterActivity.class);
+                        startActivity(intent7);
+                        return true;
+                    case R.id.navigation5:
+                        Intent intent8 = new Intent(HomeDokterActivity.this, HomeDokterActivity.class);
+                        startActivity(intent8);
+                        return true;
+                    case R.id.navigation6:
+                        Intent intent9 = new Intent(HomeDokterActivity.this, LoginActivity.class);
+                        startActivity(intent9);
+                        return true;
+                    default:
+                        Toast.makeText(getApplicationContext(),"Kesalahan Terjadi ",Toast.LENGTH_SHORT).show();
+                        return true;
+                }
             }
         });
-
-        btnLibur.setOnClickListener(new View.OnClickListener() {
+        // Menginisasi Drawer Layout dan ActionBarToggle
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
             @Override
-            public void onClick(View v) {
-                Intent liburDokter = new Intent(HomeDokterActivity.this,jadwalLiburDokter.class);
-                startActivity(liburDokter);
+            public void onDrawerClosed(View drawerView) {
+                // Kode di sini akan merespons setelah drawer menutup disini kita biarkan kosong
+                super.onDrawerClosed(drawerView);
             }
-        });
-
-        btnBatal.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent batalJadwal = new Intent(HomeDokterActivity.this,pembatalan.class
-                );
-                startActivity(batalJadwal);
+            public void onDrawerOpened(View drawerView) {
+                //  Kode di sini akan merespons setelah drawer terbuka disini kita biarkan kosong
+                super.onDrawerOpened(drawerView);
             }
-        });
+        };
+        //Mensetting actionbarToggle untuk drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        //memanggil synstate
+        actionBarDrawerToggle.syncState();
 
-        btnOffline.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent Offline = new Intent(HomeDokterActivity.this,jadwalOffline.class);
-                startActivity(Offline);
-            }
-        });
+    }
 
-        btnLive.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent Live = new Intent(HomeDokterActivity.this,jadwalLive.class);
-                startActivity(Live);
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public void onBackPressed() {
+        // Disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
